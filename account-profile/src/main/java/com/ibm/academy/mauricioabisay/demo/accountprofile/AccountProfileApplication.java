bbc731457cfd65e83b39d7b11aa595fde03a6a38
mauricioabisay <mauricioabisay.lopez@gmail.com>
2021-10-19 02:14:36 -0500

    Intial Spring project setup

---
+++ account-profile/src/main/java/com/ibm/academy/mauricioabisay/demo/accountprofile/AccountProfileApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.academy.mauricioabisay.demo.accountprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountProfileApplication.class, args);
+	}
+
+}
---
+++ account-profile/src/test/java/com/ibm/academy/mauricioabisay/demo/accountprofile/AccountProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.academy.mauricioabisay.demo.accountprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
